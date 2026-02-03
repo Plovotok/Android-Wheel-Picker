7504a3ce8a0edead45553d4724bfefb0dc6fdbc4
Plovotok <z-sizinov@mail.ru>
2026-02-03 11:27:28 +0300

    replaced dependencies

--- wheelPicker/build.gradle.kts
+++ wheelPicker/build.gradle.kts
@@ -47,8 +47,9 @@ version = properties["VERSION_NAME"].toString()
 description = properties["POM_DESCRIPTION"].toString()
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.runtime)
+//    implementation(libs.androidx.compose.ui)
 }
 
 afterEvaluate {
